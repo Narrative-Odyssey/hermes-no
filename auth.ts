24226c29a611f9264c4711ca057dd204f8fb3720
JasperLorelai <wolf.haci12@gmail.com>
2025-01-25 21:32:34 +0100

    fix: Trust host

--- auth.ts
+++ auth.ts
@@ -3,5 +3,6 @@ import NextAuth from "next-auth";
 import Discord from "@auth/core/providers/discord";
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
+    trustHost: true,
     providers: [Discord({authorization: {params: {scope: "identity"}}})],
 });
